b5d3f92b73065c5ae8e93c934f23f12346247fde
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 14:07:59 -0300

    estilizando scrollview

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+import { VStack, Image, Center, Text, Heading, Scrollview, ScrollView } from "@gluestack-ui/themed";
 
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
@@ -7,33 +7,35 @@ import { Button } from "@components/Button";
 
 export function SignIn(){
     return(
-        <VStack flex={1} bg={"$gray700"}>
-            <Image 
-                w={"$full"}
-                h={624}
-                source={BackgroundImg} 
-                defaultSource={BackgroundImg}
-                alt={"Pessoas Treinando"}
-                position={"absolute"}
-            />
-            <VStack flex={1} px={"$10"} pb={"$16"}>
-                <Center my={"$24"}>
-                    <Logo />
-                    <Text color={"$gray100"} fontSize={"$sm"}> 
-                        Treine sua mente e corpo.
-                    </Text>
-                </Center>
-                <Center gap={"$2"}>
-                    <Heading color={"$gray100"}>Acesse a conta</Heading>
-                    <Input placeholder={"E-mail"} keyboardType={"email-address"} autoCapitalize={"none"}/>
-                    <Input placeholder={"Senha"} secureTextEntry/>
-                    <Button title={"Acessar"} isLoading/>
-                </Center>
-                <Center flex={1} justifyContent="flex-end" mt={"$4"}>
-                    <Text color={"$gray100"} fontSize={"$sm"} mb={"$3"} fontFamily={"$body"}>Ainfa não tem acesso?</Text>
-                    <Button title="Criar Conta" variant="outline"/>
-                </Center>
+        <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
+            <VStack flex={1} bg={"$gray700"}>
+                <Image 
+                    w={"$full"}
+                    h={624}
+                    source={BackgroundImg} 
+                    defaultSource={BackgroundImg}
+                    alt={"Pessoas Treinando"}
+                    position={"absolute"}
+                />
+                <VStack flex={1} px={"$10"} pb={"$16"}>
+                    <Center my={"$24"}>
+                        <Logo />
+                        <Text color={"$gray100"} fontSize={"$sm"}> 
+                            Treine sua mente e corpo.
+                        </Text>
+                    </Center>
+                    <Center gap={"$2"}>
+                        <Heading color={"$gray100"}>Acesse a conta</Heading>
+                        <Input placeholder={"E-mail"} keyboardType={"email-address"} autoCapitalize={"none"}/>
+                        <Input placeholder={"Senha"} secureTextEntry/>
+                        <Button title={"Acessar"} isLoading/>
+                    </Center>
+                    <Center flex={1} justifyContent="flex-end" mt={"$4"}>
+                        <Text color={"$gray100"} fontSize={"$sm"} mb={"$3"} fontFamily={"$body"}>Ainfa não tem acesso?</Text>
+                        <Button title="Criar Conta" variant="outline"/>
+                    </Center>
+                </VStack>
             </VStack>
-        </VStack>
+        </ScrollView>
     )
 }

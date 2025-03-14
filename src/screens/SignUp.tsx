03b492f9c1aa1627aa79f9850d4c7cd3f5fd41aa
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 14:13:23 -0300

    criação da tela de SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,8 +5,8 @@ import Logo from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function SignIn(){
-    return(
+export function SignUp(){
+    return( 
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} bg={"$gray700"}>
                 <Image 
@@ -24,16 +24,17 @@ export function SignIn(){
                             Treine sua mente e corpo.
                         </Text>
                     </Center>
-                    <Center gap={"$2"}>
-                        <Heading color={"$gray100"}>Acesse a conta</Heading>
+                    <Center gap={"$2"} flex={1}>
+                        <Heading color={"$gray100"}>Crie sua conta</Heading>
+
+                        <Input placeholder={"Nome"} />
                         <Input placeholder={"E-mail"} keyboardType={"email-address"} autoCapitalize={"none"}/>
                         <Input placeholder={"Senha"} secureTextEntry/>
-                        <Button title={"Acessar"}/>
+
+                        <Button title={"Criar e acessar"}/>
                     </Center>
-                    <Center flex={1} justifyContent="flex-end" mt={"$4"}>
-                        <Text color={"$gray100"} fontSize={"$sm"} mb={"$3"} fontFamily={"$body"}>Ainfa não tem acesso?</Text>
-                        <Button title="Criar Conta" variant="outline"/>
-                    </Center>
+
+                    <Button title="Voltar para o login" variant="outline" mt={"$12"}/>
                 </VStack>
             </VStack>
         </ScrollView>

be9a0823ce872c0de2c7ad1dc00b26819d71ad6d
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 16:53:44 -0300

    compontnete de foto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,10 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader(){
     return(
-        <HStack bg={"$gray600"} pt={"$16"} pb={"$5"} px={"$8"} alignItems={"center"}>
+        <HStack bg={"$gray600"} pt={"$16"} pb={"$5"} px={"$8"} alignItems={"center"} gap={"$4"}>
+            <UserPhoto source={{uri: "https://avatars.githubusercontent.com/u/45099916?v=4"}} alt="Imagem do Usuário" w={"$16"} h={"$16"}/>
             <VStack>
                 <Text color={"$gray100"} fontSize={"$sm"}>Olá, </Text>
                 <Heading color={"$gray100"} fontSize={"$md"}>Cesar August</Heading>

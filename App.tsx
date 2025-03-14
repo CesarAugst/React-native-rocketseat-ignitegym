eaccdc3b22307f53657e1f5c8021fa1ebb095e3d
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 14:46:58 -0300

    cor de fundo

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/ro
 import {Center, GluestackUIProvider, Text} from "@gluestack-ui/themed";
 import {config} from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -11,7 +11,7 @@ export default function App() {
     return (
         <GluestackUIProvider config={config}>
                 <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
-            { fontsLoaded ? <SignUp />: <Loading/>}
+            { fontsLoaded ? <Routes />: <Loading/>}
         </GluestackUIProvider>
     );
 }

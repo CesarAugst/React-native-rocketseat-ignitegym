cab94b264e41f7216744ea08dd208efd5e6d0bcf
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 09:34:10 -0300

    screen SignIn

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/ro
 import {Center, GluestackUIProvider, Text} from "@gluestack-ui/themed";
 import {config} from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -10,7 +11,7 @@ export default function App() {
     return (
         <GluestackUIProvider config={config}>
                 <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
-            { !fontsLoaded ? <Center flex={1} bg={"$gray700"}><Text color={"white"}>Home</Text></Center>: <Loading/>}
+            { fontsLoaded ? <SignIn />: <Loading/>}
         </GluestackUIProvider>
     );
 }

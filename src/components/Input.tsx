c545eff61a4c0a362a7024d9e1a1d640aca72f58
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-03-14 10:17:11 -0300

    evento de focus no input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,17 @@ type Props = ComponentProps<typeof InputField>
 
 export function Input({...rest}: Props){
     return(
-        <GluestackInput bg={"$gray700"} h={"$14"} px={"$4"} borderWidth={"$0"} borderRadius={"$md"}>
+        <GluestackInput 
+            bg={"$gray700"} 
+            h={"$14"} 
+            px={"$4"} 
+            borderWidth={"$0"} 
+            borderRadius={"$md"} 
+            $focus={{
+                borderWidth: 1,
+                borderColor: "$green500"
+            }}
+        >
             <InputField color={"$white"} fontFamily={"$body"} placeholderTextColor={"$gray300"} {...rest}/>
         </GluestackInput>
     )

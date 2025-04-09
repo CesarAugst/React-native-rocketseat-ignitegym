e8d481f5f42e8101c7d1fb0994bb9e8905ca482f
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-04-09 09:28:24 -0300

    implementando perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView } from "react-native";
 
 export function Profile(){
     return(
-        <Center flex={1}>
-            <Text>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title={"Perfil"} />
+
+            <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+                <Center mt={"$6"} px={"$10"}>
+                    <UserPhoto 
+                        source={{uri: "https://avatars.githubusercontent.com/u/45099916?v=4"}} 
+                        alt={"Foto du usuário"}
+                        size={"xl"}
+                    />
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }

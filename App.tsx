5c1fc71a420fea62aeb62249f9e25de72c5a0f0d
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-04-11 13:46:07 -0300

    utlizando contexto na aplicação

--- App.tsx
+++ App.tsx
@@ -4,14 +4,22 @@ import {Center, GluestackUIProvider, Text} from "@gluestack-ui/themed";
 import {config} from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
+import { AuthContext } from '@contexts/AuthContext';
 
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
     return (
         <GluestackUIProvider config={config}>
-                <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
-            { fontsLoaded ? <Routes />: <Loading/>}
+            <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
+            <AuthContext.Provider value={{
+                id: '1',
+                name: 'Cesar',
+                email: 'cesar@email.com',
+                avatar: 'cesar.png'
+            }}>
+                { fontsLoaded ? <Routes />: <Loading/>}
+            </AuthContext.Provider>
         </GluestackUIProvider>
     );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,16 @@ import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
+import { useContext } from "react";
+import { AuthContext } from "@contexts/AuthContext";
 
 
 export function Routes(){
     const theme = DefaultTheme
     theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
+    const contextData = useContext(AuthContext);
+    
     return(
         <Box flex={1} bg={"$gray700"}>
             <NavigationContainer theme={theme}>

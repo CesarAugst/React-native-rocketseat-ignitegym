3bf6dc3206486368e9e84042df52cfac01369a64
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-04-11 13:51:25 -0300

    UserDTO

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import {StatusBar} from 'react-native';
-import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/roboto';
-import {Center, GluestackUIProvider, Text} from "@gluestack-ui/themed";
-import {config} from "./config/gluestack-ui.config";
+import { StatusBar } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
 import { AuthContext } from '@contexts/AuthContext';
@@ -11,14 +11,16 @@ export default function App() {
 
     return (
         <GluestackUIProvider config={config}>
-            <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
+            <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true} />
             <AuthContext.Provider value={{
-                id: '1',
-                name: 'Cesar',
-                email: 'cesar@email.com',
-                avatar: 'cesar.png'
+                user: {
+                    id: '1',
+                    name: 'Cesar',
+                    email: 'cesar@email.com',
+                    avatar: 'cesar.png'
+                }
             }}>
-                { fontsLoaded ? <Routes />: <Loading/>}
+                {fontsLoaded ? <Routes /> : <Loading />}
             </AuthContext.Provider>
         </GluestackUIProvider>
     );

01868e85d659bc4747c33aea17c3c852553b6008
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-04-11 16:44:17 -0300

    homeHeader carregando dados a partir do context

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,14 +1,18 @@
 import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
+import { useAuth } from "@hooks/useAuth";
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png"
 
 export function HomeHeader(){
+    const {user} = useAuth();
+
     return(
         <HStack bg={"$gray600"} pt={"$16"} pb={"$5"} px={"$8"} alignItems={"center"} gap={"$4"}>
-            <UserPhoto source={{uri: "https://avatars.githubusercontent.com/u/45099916?v=4"}} alt="Imagem do Usuário" w={"$16"} h={"$16"}/>
+            <UserPhoto source={user.avatar ? {uri: "https://avatars.githubusercontent.com/u/45099916?v=4"} : defaultUserPhotoImg } alt="Imagem do Usuário" w={"$16"} h={"$16"}/>
             <VStack flex={1}>
                 <Text color={"$gray100"} fontSize={"$sm"}>Olá, </Text>
-                <Heading color={"$gray100"} fontSize={"$md"}>Cesar August</Heading>
+                <Heading color={"$gray100"} fontSize={"$md"}>{user.name}</Heading>
             </VStack>
             <Icon as={LogOut} color="$gray200" size="xl"/>
         </HStack>

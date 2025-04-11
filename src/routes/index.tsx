34194155970b8a35b04f8c70ec256d4366f8b18b
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-04-11 14:20:13 -0300

    criando hooks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,18 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
-import { useContext } from "react";
-import { AuthContext } from "@contexts/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
 
-export function Routes(){
+export function Routes() {
+    const { user } = useAuth();
+
     const theme = DefaultTheme
-    theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+    theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
 
-    const contextData = useContext(AuthContext);
-    
-    return(
+    return (
         <Box flex={1} bg={"$gray700"}>
             <NavigationContainer theme={theme}>
                 <AuthRoutes />
